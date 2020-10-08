302e6ce36632d61d0bc357a01d77df9d56ac4e2f
allenorton <norton.allene@gmail.com>
2020-10-08 13:43:00 -0500

    created callback controller, configured routes, added middleware, setup CORS, installed devise, created and added to user model

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :omniauthable, :omniauth_providers => [:google_oauth2]
+  def self.from_omniauth(auth)
+    # Either create a User record or update it based on the provider (Google) and the UID   
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.token = auth.credentials.token
+      user.expires = auth.credentials.expires
+      user.expires_at = auth.credentials.expires_at
+      user.refresh_token = auth.credentials.refresh_token
+      user.email = auth.info.email
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.location = auth.info.location
+      user.image = auth.info.image
+    end
+  end
+end
---
+++ db/migrate/20201008182812_add_data_to_users.rb
@@ -0,0 +1,8 @@
+class AddDataToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :location, :string
+    add_column :users, :image, :string
+  end
+end
