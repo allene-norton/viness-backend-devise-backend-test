aec3482435c40a018df64825a4b8af6ab30cad8d
allenorton <norton.allene@gmail.com>
2020-10-10 18:32:37 -0500

    current_user not working, reverting master to previous commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
-    def current_user
-        token = request.headers["Authorization"].to_s
-        User.find_for_database_authentication(authentication_token: token)
-    end
+    # def current_user
+    #     token = request.headers["Authorization"].to_s
+    #     User.find_for_database_authentication(authentication_token: token)
+    # end
 end
--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -2,12 +2,15 @@ class CurrentUserController < ApplicationController
     before_action :authenticate_user!
     def index
         if user_signed_in?
-            render json: current_user
+            search_id = current_user.id 
+            user = User.where(id: search_id).first
+            render json: user
             puts current_user
             puts user_signed_in?
         else
             render json: {}, status: 401
             puts user_signed_in?
+            puts current_user
         end
     end
 end
